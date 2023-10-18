804b3fadd7b0b31c2e112519db57ef504a376801
n810tran <nathaniel.k.tran@gmail.com>
2023-10-18 01:59:40 -0700

    added controllers and routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction,
+} = require('../controllers/thoughtController');
+
+// Define routes
+router.get('/', getAllThoughts);
+router.get('/:id', getThoughtById);
+router.post('/', createThought);
+router.put('/:id', updateThought);
+router.delete('/:id', deleteThought);
+router.post('/:thoughtId/reactions', addReaction);
+router.delete('/:thoughtId/reactions/:reactionId', deleteReaction);
+
+module.exports = router;
